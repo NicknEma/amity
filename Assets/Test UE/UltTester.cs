7b94586afe13e7f3568f661ab86541a5069a745d
NicknEma <emanuele.rovini@gmail.com>
2021-05-05 15:07:00 +0200

    Imported UltEvents

---
+++ Assets/Test UE/UltTester.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UltEvents;
+
+namespace Amity
+{
+    public class UltTester : MonoBehaviour
+    {
+        public UltEvent<int, int> ev;
+
+        public void Test(int n, int m) { }
+    }
+}
