13aeb89e8617ce67946632f368b11e45a922fe34
NicknEma <emanuele.rovini@gmail.com>
2021-06-03 00:41:09 +0200

    Created GameFeelController

---
+++ Assets/GameFeelController.cs
@@ -0,0 +1,13 @@
+using UnityEngine.Rendering;
+using UnityEngine;
+using Cinemachine;
+
+namespace Amity
+{
+    public class GameFeelController : MonoBehaviour
+    {
+        public Camera mainCamera;
+        public CinemachineVirtualCamera virtualCamera;
+        public Volume volume;
+    }
+}
