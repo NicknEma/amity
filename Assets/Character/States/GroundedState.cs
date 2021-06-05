50dad4431d9f285eaf1524ec16c32967b5ed289a
NicknEma <emanuele.rovini@gmail.com>
2021-06-05 21:08:36 +0200

    Finished slope movement implementation

--- Assets/Character/States/GroundedState.cs
+++ Assets/Character/States/GroundedState.cs
@@ -25,6 +25,9 @@ namespace Amity
 				character.runSpeed * tangent.x * character.CurrentHorizontalInput,
 				character.runSpeed * tangent.y * character.CurrentHorizontalInput);
 
+			if (Input.GetKeyDown(KeyCode.I))
+				Debug.Log($"{character.runSpeed * tangent.x}, {character.runSpeed * tangent.y}");
+
 			character.rigidbody.velocity = speed;
 
 			SetMaterial(character.CurrentHorizontalInput);
@@ -38,7 +41,7 @@ namespace Amity
 
 		private Vector2 GetGroundNormal() {
 			float distance = 0.5f;
-			Vector2 origin = character.transform.position;
+			Vector2 origin = (Vector2) character.transform.position - new Vector2(0.5f, 0.0f);
 			RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, distance, 1 << LayerMask.NameToLayer("Ground"));
 
 			Vector2 normal = hit.normal == Vector2.zero ? Vector2.up : hit.normal.normalized;
